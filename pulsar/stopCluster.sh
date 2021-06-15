0d4a8591304cab518114cd0d7e193d5d3f077144
perayb <aybthebest@yahoo.com>
2021-06-15 11:06:38 +0800

    add stop方法

---
+++ pulsar/stopCluster.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 停止服务
+docker compose -f docker-compose-cluster.yml down
---
+++ pulsar/stopStandalone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 删除现有容器(如果存在)
+docker rm -f pulsar-standalone
+docker rm -f pulsar-manager
+
